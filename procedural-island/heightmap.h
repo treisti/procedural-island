d4f92e0e9627b40aba19e58e2aaaa13bd35fb658
treisti <roillettri@eisti.eu>
2018-03-12 16:02:56 +0100

    Heightmap diamond-square generation

--- procedural-island/heightmap.h
+++ procedural-island/heightmap.h
@@ -1,16 +1,23 @@
 #ifndef HEIGHTMAP_H
 #define HEIGHTMAP_H
 
+#include <math.h>
 #include <vector>
+#include "random.h"
 
 class Heightmap
 {
 public:
     Heightmap(int _xSize, int _ySize);
     void clear(); // Clear the heightmap
+    void diamondSquare(double smoothness); // Heightmap generation using diamond-square algorithm
 private:
-    std::vector<std::vector<double> *> * map;
+    std::vector<std::vector<double> > map;
     int xSize, ySize;
+
+    // Diamond-square algorith fonctions
+    double square(int x, int y, int step, std::vector<std::vector<double> > map);
+    double diamond(int x, int y, int step, std::vector<std::vector<double> > map, int size);
 };
 
 #endif // HEIGHTMAP_H
